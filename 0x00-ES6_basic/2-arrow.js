e93025c07a7d6a5f17bf3501a9a6a0ee87901dbc
Kehinde Opaleye <kenny@LENOVO>
2023-10-04 17:05:52 +0100

    Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,11 @@
+#!/usr/bin/node
 export default class getNeighborhoodsList {
   constructor() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   }
 
-  addNeighborhood = (newNeighborhood) => {
+  addNeighborhood(newNeighborhood) {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
-  };
+  }
 }
-
