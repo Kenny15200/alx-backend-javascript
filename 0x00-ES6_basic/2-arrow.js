da628043830e9594feb898513e99337d9cfe0e31
Kehinde Opaleye <kenny@LENOVO>
2023-10-04 15:22:22 +0100

    const or let

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+
+    addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
+
