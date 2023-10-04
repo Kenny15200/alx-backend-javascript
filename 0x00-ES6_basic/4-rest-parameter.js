ff7c4d74463e8e6a54cb3f0b9370e217ec2d4dda
Kehinde Opaleye <kenny@LENOVO>
2023-10-04 18:19:22 +0100

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-export default function returnHowManyArguments(...theArgs) {
-	return args.length;
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
