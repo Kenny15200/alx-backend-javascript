5f910417f8ac3e20262efe6fed8d2d3918f3852a
Kehinde Opaleye <kenny@LENOVO>
2023-12-20 19:28:01 +0100

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node 
+
+// Define the displayMessage function
+function displayMessage(message) {
+  console.log(message); // Print the message to STDOUT
+}
+
+// Export the function to make it accessible from other files
+module.exports = displayMessage;
+
---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,6 @@
+// Import the displayMessage function from 0-console.js
+const displayMessage = require('./0-console');
+
+// Call the displayMessage function with a string argument
+displayMessage("Hello NodeJS!");
+
