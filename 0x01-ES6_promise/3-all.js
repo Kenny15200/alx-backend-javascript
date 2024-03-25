5f07c5a6cea3a5f5c92a05790d781715782db81d
Kehinde Opaleye <kenny@LENOVO>
2024-03-25 13:34:46 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js';
+
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.error("Signup system offline");
+  }
+}
+
+export default handleProfileSignup;
