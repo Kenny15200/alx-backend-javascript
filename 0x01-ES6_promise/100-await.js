bb1fffd16bb4c2ab8310676fc3ed318ebccd66fe
Kehinde Opaleye <kenny@LENOVO>
2024-03-25 16:52:20 +0100

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js';
+
+async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo: photoResponse,
+      user: userResponse
+    };
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    return {
+      photo: null,
+      user: null
+    };
+  }
+}
+
+export default asyncUploadUser;
