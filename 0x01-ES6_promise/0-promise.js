353dfce24571100b1721e4208f5bbc8b05d06e48
Kehinde Opaleye <kenny@LENOVO>
2024-03-25 12:06:00 +0100

    Keep Every Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // I can resolve with a simple dummy response.
-    const response = { status: 200, body: 'dummy-response' };
+  return new Promise((resolve) => { // Remove 'reject' parameter as it's not used
+    // As per the task description, we are not making any actual API calls.
+    // We can resolve with a simple dummy response.
+    const response = {
+      status: 200,
+      body: 'dummy-response',
+    };
     resolve(response);
   });
 }
 
 export default getResponseFromAPI;
-
