d6c24992e4069a15f9455c3f0a72e068be3d7473
Kehinde Opaleye <kenny@LENOVO>
2024-04-06 18:50:01 +0100

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Custom toString() method
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
+export default Airport;
