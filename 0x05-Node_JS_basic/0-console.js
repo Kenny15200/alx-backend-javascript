54d2120a6a2365ccf6db56cccc00619c7fc07f76
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-27 06:01:11 -0500

    update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 /**
  * Display a message to STDOUT.
  * @param {string} message - The message to be displayed.
@@ -6,7 +6,5 @@
 function displayMessage(message) {
   console.log(message);
 }
-
 // Export the displayMessage function
 module.exports = displayMessage;
-
