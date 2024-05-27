e75e49ad06eb93bf094e50bc0850107b37792c16
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-27 06:31:12 -0500

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,15 +3,15 @@ const readline = require('readline');
 
 // Create an interface to read input from stdin and output to stdout
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 // Display the initial message
 console.log('Welcome to Holberton School, what is your name?');
 // Listen for input from the user
 rl.on('line', (input) => {
   // Display the user's name
-  console.log(`Your name is: ${input}`);	
+  console.log(`Your name is: ${input}`);
   // Close the readline interface
   rl.close();
 });
