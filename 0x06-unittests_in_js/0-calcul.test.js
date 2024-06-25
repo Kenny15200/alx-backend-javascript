7f5e396954eaa571ac51e9a7f88bd05e6439231c
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-06-25 14:33:20 -0500

    Calculus

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,22 @@
-// test 0-calcul file
-
+// 0-calcul.test.js
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum of rounded numbers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+    it('should return 4 for (1, 3)', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return 5 for (1, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 for (1.2, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 for (1.5, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
 });
 
